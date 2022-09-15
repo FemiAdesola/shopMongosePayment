06ac9ac9f813b473b0f944ece3ee01a52dd8ba66
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-09-15 17:21:31 +0300

    Added get product from admin function with sequ..

--- controllers/admin.js
+++ controllers/admin.js
@@ -70,13 +70,16 @@ exports.postEditProduct = (req, res, next)=>{
 
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('admin/products', {
+     Product.findAll()
+     .then((products) => {
+            res.render('admin/products', {
             prod: products,
             pageTitle: 'Admin Products',
             path: '/admin/products',
         });
-    });
+        })
+        .catch(error =>
+            console.log(error));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
