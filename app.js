371da5486737887b2d08e3c2a3b98cda00ee9012
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-09-16 12:00:44 +0300

    set up user find by id in app.js

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const errorController = require('./controllers/error')
 // for database
 // const db = require('./util/database');
 const sequelize = require('./util/database');
+
+const Product = require('./models/product');
+const User = require('./models/user');
+
 // from express
 const app = express();
 
@@ -30,16 +34,42 @@ app.set('views', 'pages');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// for user
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user; 
+            next();
+        })
+        .catch(error =>console.log(error));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// This form a one to many relationship
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 // this function create appropriate table
-sequelize.sync()
+sequelize
+    // .sync({ force: true })
+    .sync()
     .then(result => {
-        console.log(result);
-        app.listen(3000)
+       return User.findByPk(1);
+        // console.log(result);
+    })
+    .then(user => {
+        if (!user) {
+           return User.create({name:'Femi', email:'ade@oyin.com'})
+        }
+        return user;
+    })
+    .then(user => {
+        // console.log(user);
+        app.listen(3000);
     })
     .catch(error => {
         console.log(error);
