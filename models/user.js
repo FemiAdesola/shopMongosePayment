b26d2e9f3a3a4c0bf80c4f0d1f89dae47612b5a5
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-09-21 15:11:25 +0300

    update shooping cart with mongoose

--- models/user.js
+++ models/user.js
@@ -18,6 +18,27 @@ const userSchema = new Schema({
         }]
     },
 });
+
+userSchema.methods.addToCart = function(productInCart){
+    const cartProductIndex = this.cart.items.findIndex(pro => {
+            return pro.productId.toString() === productInCart._id.toString();
+        });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+        newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+        updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+        updatedCartItems.push({
+            productId: productInCart._id,
+            quantity: newQuantity
+        });
+    }
+    const updatedCart = { items: updatedCartItems};
+    this.cart=updatedCart; 
+    return this.save()
+};
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
