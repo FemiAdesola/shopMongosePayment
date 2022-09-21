0f9c472accf212c1f4e30a8402d039936473c2b4
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-09-21 14:37:02 +0300

    update user model and user functionality in app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const errorController = require('./controllers/error');
 // for database
 const mongoConnect = require('./util/database.js').mongoConnect;
 
-// // User
-// const User = require('./models/user');
+// User
+const User = require('./models/user');
 
 // from express
 const app = express();
@@ -25,20 +25,15 @@ app.set('views', 'pages');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// // for user
-// app.use((req, res, next) => {
-//     User.findById("6329cea4a48011b1de7afb96")
-//         .then(user => {
-//             req.user = new User(
-//                 user.name,
-//                 user.email,
-//                 user.cart,
-//                 user._id
-//             );
-//             next();
-//         })
-//         .catch(error =>console.log(error));
-// })
+// for user
+app.use((req, res, next) => {
+    User.findById("632af6e411a86492cb452c83")
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(error =>console.log(error));
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -47,6 +42,19 @@ app.use(errorController.get404);
 // server connection with mongoose 
 mongoose.connect('mongodb+srv://Femi:CwRbXZuHSUaMW9yH@shop.fftoabl.mongodb.net/shop?retryWrites=true&w=majority')
     .then(result => {
+        // to avoid duplicate 
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Femi',
+                    email: 'Ade@yahoo.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
         app.listen(3000)
     })
     .catch(error => {
