221e88e66ca429efd725c3399e04399b60840ef3
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-09-22 16:17:33 +0300

    creates postSignUp in auth controller

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,14 @@ const authController = require('../controllers/auth')
 const express = require('express');
 const router = express.Router();
 
+// login
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 
 router.post('/logout', authController.postLogout);
 
+// signup
 router.get('/signup', authController.getSignup);
+router.post('/signup', authController.postSignup);
 
 module.exports = router;
